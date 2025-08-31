ede1167c2d80547e838b5c668fe2b53e55fb7954
Gokul <gokulgk9402@gmail.com>
2025-08-31 18:37:53 +0530

    add create image, container, start container in github webhook

--- internal/services/git/handlers.go
+++ internal/services/git/handlers.go
@@ -3,8 +3,15 @@ package git
 import (
 	"encoding/json"
 	"fmt"
+	"os/exec"
 
+	"github.com/docker/go-connections/nat"
+	"github.com/gokuls-codes/on-the-go/internal/utils"
 	"github.com/labstack/echo/v4"
+	"github.com/moby/moby/api/types/build"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/api/types/network"
+	"github.com/moby/moby/client"
 )
 
 
@@ -28,5 +35,81 @@ func (h *Handler) gitPush(c echo.Context) error {
 	headers := c.Request().Header
 
 	fmt.Println("Headers:", headers)
-	return c.JSON(200, map[string]string{"message": "Git push successful"})
+
+	cmd := exec.Command("git", "pull")
+    cmd.Dir = "../test-docker-project"
+
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        return c.JSON(500, map[string]string{"error": fmt.Sprintf("Error executing git pull: %s", string(output))})
+    }
+
+	apiClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": "Failed to create Docker client"})
+	}
+
+	defer apiClient.Close()
+
+	buildContext, err := utils.TarDirectory("../test-docker-project")
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+
+	buildOptions := build.ImageBuildOptions{
+		Dockerfile: "Dockerfile",
+		Tags: []string{"test-docker-project"},
+		Remove: true,
+	}
+
+	response, err := apiClient.ImageBuild(c.Request().Context(), buildContext, buildOptions)
+
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+
+	decoder := json.NewDecoder(response.Body)
+	for decoder.More() {
+		var msg map[string]interface{}
+		if err := decoder.Decode(&msg); err != nil {
+			return c.JSON(500, map[string]string{"error": err.Error()})
+		}
+		if stream, ok := msg["stream"].(string); ok {
+			fmt.Fprintf(c.Response().Writer, "<div>%s</div>", stream)
+			c.Response().Flush()
+		}
+		// time.Sleep(200 * time.Millisecond)
+	}
+
+	containerResp, err := apiClient.ContainerCreate(
+		c.Request().Context(),
+		&container.Config{
+			Image: "test-docker-project",
+		},
+		&container.HostConfig{
+			PortBindings: nat.PortMap{
+				nat.Port("3000/tcp"): []nat.PortBinding{
+					{
+						HostIP:   "0.0.0.0",
+						HostPort: "3000",
+					},
+				},
+			},
+		},
+		&network.NetworkingConfig{
+		},
+		nil, "",
+	)
+
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+
+	err = apiClient.ContainerStart(c.Request().Context(), containerResp.ID, container.StartOptions{})
+	
+	if err != nil {
+		return c.JSON(500, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(200, map[string]string{"message": "Webhook successful"})
 }
