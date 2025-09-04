5fe7343a6978859710fe4fcd40eee4e74dfeaffd
Gokul <gokulgk9402@gmail.com>
2025-09-04 20:21:23 +0530

    minor changes

--- internal/web/templates/pages/new-project_templ.go
+++ internal/web/templates/pages/new-project_templ.go
@@ -62,7 +62,7 @@ func NewProjectPage() templ.Component {
 					}()
 				}
 				ctx = templ.InitializeContext(ctx)
-				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "Title")
+				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "Name")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
